8bc827f9d781304d5deaa5283fceffcff50f0cfe
Daniel Estrada <daniel.estrada98@gmail.com>
2012-06-05 20:36:27 -0400

    Episode 7

--- salesperson.rb
+++ salesperson.rb
@@ -2,9 +2,11 @@ Dir["./lib/*.rb"].each {|file| require file }
 
 
 phil = SalesPerson.new
-phil.schedule_city(Place.build("Dallas, TX"))
-phil.schedule_city(Place.build("El Paso, TX"))
+phil.schedule_city(Place.build("Herndon, Va"))
 phil.schedule_city(Place.build("Austin, TX"))
+phil.schedule_city(Place.build("Atlanta, GA"))
 phil.schedule_city(Place.build("Lubbock, TX"))
 
+puts "Phil's starting location is #{phil.cities.first}"
 puts phil.route
+
